411e3fa319f987d18ef95f94079a8ed56bbdaa55
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-09-19 13:10:20 -0300

    Aprendendo sobre props, métodos e construtores em C#

---
+++ Propriedades, Métodos e Construtores com C#/ExemploExplorando/Program.cs
@@ -0,0 +1,15 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome: "Patrick", sobrenome: "Monteiro", idade: 34);
+
+Pessoa p2 = new Pessoa(nome: "Leonardo", sobrenome: "Buta", idade: 37);
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Inglês";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+// p1.Apresentar();
