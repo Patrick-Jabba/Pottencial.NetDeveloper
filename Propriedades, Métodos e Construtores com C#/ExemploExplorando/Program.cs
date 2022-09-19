44bbfa89740e483510b02c54f6e9677dffbc7c4d
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-09-19 15:43:15 -0300

    manipulação de valores em C#

--- Propriedades, Métodos e Construtores com C#/ExemploExplorando/Program.cs
+++ Propriedades, Métodos e Construtores com C#/ExemploExplorando/Program.cs
@@ -1,15 +1,52 @@
 ﻿using ExemploExplorando.Models;
+using System.Globalization;
 
-Pessoa p1 = new Pessoa(nome: "Patrick", sobrenome: "Monteiro", idade: 34);
+string dataString = "2022-10-19 15:00";
 
-Pessoa p2 = new Pessoa(nome: "Leonardo", sobrenome: "Buta", idade: 37);
+bool sucesso = DateTime.TryParseExact(
+  dataString, "yyyy-MM-dd HH:mm", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime data);
 
-Curso cursoDeIngles = new Curso();
-cursoDeIngles.Nome = "Inglês";
-cursoDeIngles.Alunos = new List<Pessoa>();
+// Console.WriteLine(data.ToShortDateString());
+// Console.WriteLine(data.ToShortTimeString());
 
-cursoDeIngles.AdicionarAluno(p1);
-cursoDeIngles.AdicionarAluno(p2);
-cursoDeIngles.ListarAlunos();
+if(sucesso)
+{
+  Console.WriteLine($"Conversão com sucesso! Data: {data}");
+}
+else
+{
+  Console.WriteLine($"{dataString} não é uma data válida");
+}
+
+
+
+// CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+
+// decimal valorMonetario = 1582.40M;
+
+// Console.WriteLine(valorMonetario.ToString("C2"));
+
+// double porcentagem = .3421;
+
+// Console.WriteLine(porcentagem.ToString("P"));
+
+// int numero = 123456;
+
+// Console.WriteLine(numero.ToString("##-##-##"));
+
+
+
+
+// Pessoa p1 = new Pessoa(nome: "Patrick", sobrenome: "Monteiro", idade: 34);
+
+// Pessoa p2 = new Pessoa(nome: "Leonardo", sobrenome: "Buta", idade: 37);
+
+// Curso cursoDeIngles = new Curso();
+// cursoDeIngles.Nome = "Inglês";
+// cursoDeIngles.Alunos = new List<Pessoa>();
+
+// cursoDeIngles.AdicionarAluno(p1);
+// cursoDeIngles.AdicionarAluno(p2);
+// cursoDeIngles.ListarAlunos();
 
 // p1.Apresentar();
