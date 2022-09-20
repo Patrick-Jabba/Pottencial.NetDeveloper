089c599c75448647697fb8502d5d69dd66e45a53
Patrick Monteiro Fischer <monteiromonterio@gmail.com>
2022-09-20 09:12:51 -0300

    tuplas ternários desconstrução de objeto em C#

---
+++ Tuplas, Operador Ternário Descontrução de um Objeto com C#/ExemploTuplas-Ternario/Program.cs
@@ -0,0 +1,9 @@
+﻿(int, string, string, decimal) tupla = (1, "Patrick", "Monteiro", 10.5M);
+ValueTuple<int, string, string, decimal> outroExemploTupla = (1, "Leonardo", "Buta", 1.80M);
+
+var outroExemploTuplaCreate = Tuple.Create(1, "Leo", "Buta", 1.80M);
+
+Console.WriteLine($"Id: {tupla.Item1}");
+Console.WriteLine($"Nome: {tupla.Item2}");
+Console.WriteLine($"Sobrenome: {tupla.Item3}");
+Console.WriteLine($"Altura: {tupla.Item4}");
